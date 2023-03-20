f4c911ace90d7fe6f75d02ac761ec642c64e3256
Amir <amir.akbrzadh3@gmail.com>
2023-03-20 23:43:52 +0330

    store stock and star icons

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -51,4 +51,71 @@ function RightSideIcon() {
     </svg>
   );
 }
-export { Icon , LeftSideIcon , RightSideIcon};
+function Aviable() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      fill="none"
+      viewBox="0 0 11 10"
+    >
+      <circle cx="5.5" cy="5" r="5" fill="#78A962"></circle>
+      <path
+        stroke="#fff"
+        strokeLinecap="round"
+        d="M7.5 4L4.984 7 3.5 5.279"
+      ></path>
+    </svg>
+  );
+}
+function OutOfStock() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      fill="none"
+      viewBox="0 0 11 10"
+    >
+      <circle cx="5.5" cy="5" r="5" fill="#C94D3F"></circle>
+      <path
+        fill="#fff"
+        d="M6.177 5.856l.35-.35a.342.342 0 01.365-.075l.427.17a.34.34 0 01.212.313v.781a.34.34 0 01-.356.336C4.186 6.845 3.583 4.314 3.47 3.345a.339.339 0 01.339-.376h.755a.338.338 0 01.312.212l.17.427a.338.338 0 01-.073.365l-.35.35s.202 1.365 1.555 1.533z"
+      ></path>
+    </svg>
+  );
+}
+function PickedStar() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="14"
+      height="14"
+      fill="none"
+      viewBox="0 0 14 14"
+    >
+      <path
+        fill="#E9A426"
+        d="M6.911.174a.1.1 0 01.178 0l1.84 3.594a.1.1 0 00.073.053l3.987.64a.1.1 0 01.055.169l-2.85 2.86a.1.1 0 00-.027.086l.623 3.99a.1.1 0 01-.144.104l-3.6-1.826a.1.1 0 00-.091 0L3.354 11.67a.1.1 0 01-.144-.104l.623-3.99a.1.1 0 00-.028-.086L.956 4.63a.1.1 0 01.055-.17l3.987-.64a.1.1 0 00.073-.052L6.91.174z"
+      ></path>
+    </svg>
+  );
+}
+function UnPickedStar() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="14"
+      height="14"
+      fill="none"
+      viewBox="0 0 14 14"
+    >
+      <path
+        fill="#CACDD8"
+        d="M6.911.174a.1.1 0 01.178 0l1.84 3.594a.1.1 0 00.073.053l3.987.64a.1.1 0 01.055.169l-2.85 2.86a.1.1 0 00-.027.086l.623 3.99a.1.1 0 01-.144.104l-3.6-1.826a.1.1 0 00-.091 0L3.354 11.67a.1.1 0 01-.144-.104l.623-3.99a.1.1 0 00-.028-.086L.956 4.63a.1.1 0 01.055-.17l3.987-.64a.1.1 0 00.073-.052L6.91.174z"
+      ></path>
+    </svg>
+  );
+}
+export { Icon , LeftSideIcon , RightSideIcon , Aviable , OutOfStock , PickedStar , UnPickedStar};
