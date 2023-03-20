75f3fd4895b08331204f807de0ad6cdf82dad633
Amir <amir.akbrzadh3@gmail.com>
2023-03-20 23:44:47 +0330

    link products to main file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./components/Header";
+import Products from "./components/Products";
 import Hero from "./Hero";
 export default function App() {
   return (
     <div>
       <Header />
       <Hero />
+      <Products />
     </div>
   );
 }
