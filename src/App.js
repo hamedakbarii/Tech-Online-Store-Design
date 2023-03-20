9ccf2a6833c9e189d4422bb144d0fde7d96182ec
Amir <amir.akbrzadh3@gmail.com>
2023-03-20 16:15:56 +0330

    link ero section into main file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Header from "./components/Header";
-
+import Hero from "./Hero";
 export default function App() {
   return (
     <div>
       <Header />
+      <Hero />
     </div>
   );
 }
