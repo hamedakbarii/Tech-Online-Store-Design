470abfd5a8225486afd01aa48b9a8890ad9324bb
Amir <amir.akbrzadh3@gmail.com>
2023-03-20 16:29:13 +0330

    adding hover intractivity for arrow icons

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,18 +1,15 @@
+import { LeftSideIcon , RightSideIcon } from "./components/Icon";
 let Hero = () => {
     return (
         <div className="container mx-auto mt-4">
-            <div className="relative ">
-                <img src="./assets/hero-banner.png" />
+            <div className="relative overflow-hidden">
+                <img src="./assets/hero-banner.png" className="w-full" />
                 <div className="absolute w-full h-full top-0 left-0 text-2xl">
-                    <div className="absolute left-0 top-0 top-[50%] translate-y-[-50%] w-10 h-10 bg-gray-200 rounded-full flex items-center justify-center">
-                        <p>
-                            1
-                        </p>
+                    <div className="absolute left-0 top-0 top-[50%] translate-y-[-50%] hover:pr-4 w-10 h-10 bg-gray-800 hover:bg-gray-600 transation-all duration-[0.2s] cursor-pointer  rounded-r-full flex items-center justify-center">
+                        <LeftSideIcon />
                     </div>
-                    <div className="absolute right-0 top-[50%] translate-y-[-50%] w-10 h-10 bg-gray-200 rounded-full flex items-center justify-center">
-                        <p>
-                            2
-                        </p>
+                    <div className="absolute right-0 top-[50%] translate-y-[-50%] hover:pl-4 w-10 h-10 bg-gray-800 hover:bg-gray-600 transation-all duration-[0.2s] cursor-pointer  rounded-l-full flex items-center justify-center">
+                        <RightSideIcon />
                     </div>
                 </div>
             </div>
