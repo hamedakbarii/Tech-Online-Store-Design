355ca83263e7f3d1476809e6e91e9d4f4307a0ad
Amir <amir.akbrzadh3@gmail.com>
2023-03-25 18:13:24 +0430

    refactore prod container from grid box to flexbox

--- src/components/ProductsContainer.jsx
+++ src/components/ProductsContainer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const ProductsContainer = ({ children }) => {
-  return <div className="container grid grid-cols-6 p-2 gap-3">{children}</div>;
+  return <div className="container flex tablet:flex-row flex-col p-2 gap-3">{children}</div>;
 };
 
 export default ProductsContainer;
