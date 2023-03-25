a982b53ba04bc6dab7647dd8eb3df868233d19dc
Amir <amir.akbrzadh3@gmail.com>
2023-03-25 18:18:04 +0430

    responsive product banner for smaller screen

--- src/components/ProductSectionBanner.jsx
+++ src/components/ProductSectionBanner.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const ProductSectionBanner = (props) => {
   return (
     <div className="text-white relative">
-      <img className="h-full object-cover object-center" src={props.src} alt="imageProduct" />
-      <div className="absolute flex justify-center items-center flex-col top-0 left-0 right-0 bottom-0 z-10">
-        <h4 className="text-white text-center text-3xl max-w-[7rem]">
+      <img className="tablet:h-full h-[6rem] object-cover object-center w-full" src={props.src} alt="imageProduct" />
+      <div className="p-2 absolute flex flex-col tablet:justify-center items-center flex-col top-0 left-0 right-0 bottom-0 z-10">
+        <h4 className="text-white text-center text-3xl tablet:max-w-[7rem] w-full">
           {props.title}
         </h4>
         <span className="absolute bottom-3 underline">See All Products</span>
