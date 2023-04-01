e5532e0cc503a901362bb92e89c4fd50988347da
Amir <amir.akbrzadh3@gmail.com>
2023-04-02 03:51:21 +0430

    jsx: add zip logo and 6month services

--- src/components/ShoppingCart/ShoppingCart.jsx
+++ src/components/ShoppingCart/ShoppingCart.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { ZipIcon } from "../Icon";
 import ShoppingCartRouter from "./ShoppingCartRouter";
 import ShoppingCartSummerySection from "./ShoppingCartSummerySection";
 
@@ -9,6 +9,12 @@ const ShoppingCart = () => {
       <ShoppingCartRouter />
       <h1 className="mb-4 text-2xl font-semibold">Shopping Cart</h1>
       <ShoppingCartSummerySection />
+      <div className="flex items-center justify-center gap-2 mt-4">
+        <ZipIcon />
+        <a className="max-w-[12rem] text-sm leading-5 flex items-center before:h-7 before:mr-2 before:block before:content-[''] before:border before:rounded-full before:border-secondaryBlue ">
+            <span>own it now, up to 6 months interest free <span>learn more</span></span>
+        </a>
+      </div>
     </div>
   );
 };
