3c7035d32035ca4a26b596e49d62e5590489cf32
hamedakbarii <hamedkarate0@gmail.com>
2023-04-07 13:12:39 +0430

    conflict error

--- src/components/Footer/Accardion.jsx
+++ src/components/Footer/Accardion.jsx
@@ -1,66 +1,68 @@
 import { useState } from "react";
 import { ArrowDown, ArrowUp } from "../Icon";
-const Accardion = ({Data}) => {
-const [Accardion , setAccardion] = useState(Data) ;
-let FooterAccardionHandler = (id , e) => {
-    let Temp = [...Data] ; 
-    if(Accardion[id].isAccardionOpen){
-        Temp[id].isAccardionOpen = false ; 
-        Temp[id].accardionContentHeight = `${0}` ;
-    }else {
-        Temp[id].isAccardionOpen = true ; 
-        Temp[id].accardionContentHeight = `${e.currentTarget.nextElementSibling.scrollHeight}` ;
+const Accardion = ({ Data }) => {
+  const [Accardion, setAccardion] = useState(Data);
+  let FooterAccardionHandler = (id, e) => {
+    let Temp = [...Data];
+    if (Accardion[id].isAccardionOpen) {
+      Temp[id].isAccardionOpen = false;
+      Temp[id].accardionContentHeight = `${0}`;
+    } else {
+      Temp[id].isAccardionOpen = true;
+      Temp[
+        id
+      ].accardionContentHeight = `${e.currentTarget.nextElementSibling.scrollHeight}`;
     }
-setAccardion(Temp) ; 
-}
-    return (
-        <>
-            {
-                Accardion && Accardion.map((item , index)=>
-                    <>
-                        <div className="flex justify-between items-center p-2" onClick={(e)=>{FooterAccardionHandler(index , e)}}>
-                            <h3 className="text-white">{item.title}</h3>
-                            <ArrowDown />
-                        </div>
-                        {
-                            item.isAccardionOpen ?                             
-                                <div style={{transitionDuration:`${item.accardionContentHeight*3}ms`,height:`${Number(item.accardionContentHeight)}px`}} className={`flex flex-col text-white gap-2 pl-4 overflow-hidden transition-all [&>*:last-child]:pb-4`} key="acc"> 
-                            {
-                                item.accardionContent.map((accardion)=>
-                                    <a>
-                                        <span>
-                                            {
-                                                accardion
-                                            }
-                                        </span>
-                                    </a>
-                                )
-
-                            }
-                                </div> 
-                                :                             
-                                <div style={{transitionDuration:`${800}ms`,height:`0px`}} className={`flex flex-col text-white gap-2 pl-4 overflow-hidden transition-all [&>*:last-child]:pb-4`} key="acc"> 
-                                {
-                                    item.accardionContent.map((accardion)=>
-                                        <a>
-                                            <span>
-                                                {
-                                                    accardion
-                                                }
-                                            </span>
-                                        </a>
-                                    )
-                                }
-                            </div>
-                        }
-                        
-                    </>
-                )
-            }
-        </>
-    )
-}
-export default Accardion ; 
+    setAccardion(Temp);
+  };
+  return (
+    <>
+      {Accardion &&
+        Accardion.map((item, index) => (
+          <>
+            <div
+              className="flex justify-between items-center p-2"
+              onClick={(e) => {
+                FooterAccardionHandler(index, e);
+              }}
+            >
+              <h3 className="text-white">{item.title}</h3>
+              <ArrowDown />
+            </div>
+            {item.isAccardionOpen ? (
+              <div
+                style={{
+                  transitionDuration: `${item.accardionContentHeight * 3}ms`,
+                  height: `${Number(item.accardionContentHeight)}px`,
+                }}
+                className={`flex flex-col text-white gap-2 pl-4 overflow-hidden transition-all [&>*:last-child]:pb-4`}
+                key="acc"
+              >
+                {item.accardionContent.map((accardion) => (
+                  <a>
+                    <span>{accardion}</span>
+                  </a>
+                ))}
+              </div>
+            ) : (
+              <div
+                style={{ transitionDuration: `${800}ms`, height: `0px` }}
+                className={`flex flex-col text-white gap-2 pl-4 overflow-hidden transition-all [&>*:last-child]:pb-4`}
+                key="acc"
+              >
+                {item.accardionContent.map((accardion) => (
+                  <a>
+                    <span>{accardion}</span>
+                  </a>
+                ))}
+              </div>
+            )}
+          </>
+        ))}
+    </>
+  );
+};
+export default Accardion;
 /*
 
               Accardion.map((item , id)=>
