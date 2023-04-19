8cac2758b3bd877e8776da75adc2ce0a5558e32b
Amir <amir.akbrzadh3@gmail.com>
2023-04-19 13:56:01 +0430

    add pointer chaser

--- src/App.js
+++ src/App.js
@@ -10,14 +10,26 @@ export default function App() {
   let routes = useRoutes(router);
   const [ProductOnFocus , setProductOnFocus] = useState(null) ; 
   const storing = [ProductOnFocus , setProductOnFocus] ; 
+  const [pointerPos , setPointerPos] = useState({
+    clientX : 0 , 
+    clientY : 0 , 
+  })
   return (
     <ProductToBuy.Provider value={storing}>
-      <div className="relative">
+      <div className="relative" onPointerMove={(e)=>{
+        setPointerPos({
+          clientX : e.pageX ,
+          clientY : e.pageY
+        })  ;
+        console.log(e)
+      }}>
         <Header />
 
         {routes}
 
         <Footer />
+        <span className="absolute w-[20px] h-[20px] rounded-full z-20 bg-[#00000045]" style={{left : (pointerPos.clientX - 10) , top : (pointerPos.clientY - 10)}}></span>
+        <span className="absolute w-[6px] h-[6px] rounded-full z-20 bg-black" style={{left : (pointerPos.clientX - 3) , top : (pointerPos.clientY - 3)}}></span>
       </div>
     </ProductToBuy.Provider>
   );
