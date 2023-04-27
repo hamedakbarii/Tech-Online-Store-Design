29f73cdc6e0b31fc66c7f80f88a6adbb207d0938
majid-amini <majidamini_s@yahoo.com>
2023-04-27 20:09:32 +0430

    image size for tablet screen updated

--- src/components/Products/ProductSectionBanner.jsx
+++ src/components/Products/ProductSectionBanner.jsx
@@ -4,11 +4,11 @@ const ProductSectionBanner = (props) => {
   return (
     <div className="text-white relative">
       <img
-        className="h-[6rem] w-full md:h-full"
+        className="h-[6rem] w-full md:h-[300px] md:min-w-[30vw]"
         src={props.src}
         alt="imageProduct"
       />
-      <div className="p-2 absolute flex items-center flex-col top-0 left-0 right-0 bottom-0 z-10 md:relative">
+      <div className="p-2 absolute flex items-center flex-col top-0 left-0 right-0 bottom-0 z-10 md:absolute md:top-[40%]">
         <h4 className="text-white text-center text-3xl  w-full">
           {props.title}
         </h4>
